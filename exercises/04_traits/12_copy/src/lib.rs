9b72125f13bbca8de5afb13e384d017feb53ac92
lisuxiaoqi <lisuxiaoqi@gmail.com>
2025-10-15 17:18:07 +0800

    04

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +11,16 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        WrappingU32 {
+            value: self.value.wrapping_add(rhs.value)
+        }
     }
 }
 
@@ -21,5 +34,15 @@ mod tests {
         let y = WrappingU32::new(31);
         let z = WrappingU32::new(u32::MAX);
         assert_eq!(x + y + y + z, WrappingU32::new(103));
+    }
+
+    #[test]
+    fn test_round() {
+        let x = u8::MAX;
+        println!("max u8: {}", x);
+        let x1 = x.wrapping_add(1u8);
+        println!("x1 u8: {}", x1);
+        let x2 = x.wrapping_add(2u8);
+        println!("x2 u8: {}", x2);
     }
 }
--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    isDefused: bool,
+}
+impl DropBomb {
+    fn new() -> Self {
+        Self {
+            isDefused: false,
+        }
+    }
+    fn defuse(&mut self) {
+        self.isDefused = true;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.isDefused {
+            panic!("panic")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
